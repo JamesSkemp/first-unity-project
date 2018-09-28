e1f23e15d22da8c91f44f4eb500565b7742564c6
James Skemp <strivinglife@gmail.com>
2018-09-28 17:54:34 -0500

    Add functionality to move player with the keyboard

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	// As a public variable will show up in the editor as a field.
+	public float speed;
+
+	private Rigidbody2D rb2d;
+
+	private void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate()
+	{
+		// Grabs cursor keys from keyboard.
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+		rb2d.AddForce(movement * speed);
+	}
+}
