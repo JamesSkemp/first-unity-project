202d3514d8512a2ec00d1da7da919b915afc85ef
James Skemp <strivinglife@gmail.com>
2018-09-28 21:39:23 -0500

    Add functionality for camera to track player position
    Not set as a child so it doesn't rotate when the player does.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	private void LateUpdate()
+	{
+		// Replicates what would happen if the camera were a child of the player.
+		// However, doesn't rotate like it would.
+		transform.position = player.transform.position + offset;
+	}
+}
